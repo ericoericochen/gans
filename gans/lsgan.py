be15b73d8e414931551350ed1b8033effd917364
Eric Chen <deeplearningericchen.com>
2025-01-13 03:02:44 +0000

    lsgan implemented

--- gans/lsgan.py
+++ gans/lsgan.py
@@ -15,48 +15,63 @@ import imageio.v2 as imageio
 
 
 class Generator(nn.Module):
-    def __init__(self):
+    def __init__(self, latent_dim: int):
         super().__init__()
+
+        def block(in_channels: int, out_channels: int, kernel_size: int, **kwargs):
+            layers = [
+                nn.ConvTranspose2d(in_channels, out_channels, kernel_size, **kwargs),
+                nn.BatchNorm2d(out_channels),
+                nn.ReLU(inplace=True),
+            ]
+            return nn.Sequential(*layers)
+
         self.generator = nn.Sequential(
-            nn.Linear(100, 256),
-            nn.ReLU(inplace=True),
-            nn.Linear(256, 1024),
-            nn.ReLU(inplace=True),
-            nn.Linear(1024, 784),
+            block(latent_dim, 1024, 4, stride=1, bias=False),
+            block(1024, 512, 4, stride=2, padding=1, bias=False),
+            block(512, 256, 4, stride=2, padding=1, bias=False),
+            block(256, 128, 4, stride=2, padding=1, bias=False),
+            nn.ConvTranspose2d(128, 1, 4, stride=2, padding=1),
         )
 
     def forward(self, z: torch.Tensor):
         x = self.generator(z)
-        x = x.view(-1, 1, 28, 28)
-        x = x.sigmoid()
+        x = x.tanh()
         return x
 
 
 class Discriminator(nn.Module):
     def __init__(self, p: float = 0.0):
         super().__init__()
+
+        def block(in_channels: int, out_channels: int, kernel_size: int, **kwargs):
+            norm = kwargs.pop("norm", True)
+            layers = [nn.Conv2d(in_channels, out_channels, kernel_size, **kwargs)]
+            if norm:
+                layers.append(nn.BatchNorm2d(out_channels))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            layers.append(nn.Dropout2d(p))
+            return nn.Sequential(*layers)
+
         self.discriminator = nn.Sequential(
-            nn.Linear(784, 1024),
-            nn.ReLU(inplace=True),
-            nn.Dropout(p),
-            nn.Linear(1024, 256),
-            nn.ReLU(inplace=True),
-            nn.Dropout(p),
-            nn.Linear(256, 1),
+            block(1, 128, 4, stride=2, padding=1, norm=False),
+            block(128, 256, 3, stride=2, padding=1, bias=False),
+            block(256, 512, 4, stride=2, padding=1, bias=False),
+            block(512, 1024, 4, stride=2, padding=1, bias=False),
+            nn.Conv2d(1024, 1, 4, stride=1, padding=0),
         )
 
     def forward(self, x: torch.Tensor):
-        b = x.shape[0]
-        x = x.view(b, -1)
         logit = self.discriminator(x).view(-1)
         return logit
 
 
 # Parse arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("--lr", type=float, default=1e-3)
+parser.add_argument("--latent_dim", type=int, default=100)
+parser.add_argument("--lr", type=float, default=0.0002)
 parser.add_argument("--bs", type=int, default=128)
-parser.add_argument("--p", type=float, default=0.5)
+parser.add_argument("--p", type=float, default=0.3)
 parser.add_argument("--seed", type=int, default=0)
 parser.add_argument("--epochs", type=int, default=10)
 parser.add_argument("--k", type=int, default=1)
@@ -74,27 +89,35 @@ dataset = MNIST(
     root=ROOT,
     train=True,
     download=True,
-    transform=v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)]),
+    transform=v2.Compose(
+        [
+            v2.ToImage(),
+            v2.ToDtype(torch.float32, scale=True),
+            v2.Resize((64, 64)),
+            v2.Normalize((0.5,), (0.5,)),
+        ]
+    ),
 )
 dataloader = DataLoader(dataset, batch_size=args.bs, shuffle=True)
 
 # Models
-generator = Generator().to(device)
+generator = Generator(latent_dim=args.latent_dim).to(device)
 discriminator = Discriminator(p=args.p).to(device)
 
 # Training setup
 timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-output_dir = os.path.join(os.path.dirname(__file__), "../outputs/gan", timestamp)
+output_dir = os.path.join(os.path.dirname(__file__), "../outputs/lsgan", timestamp)
 os.makedirs(output_dir, exist_ok=True)
 os.makedirs(os.path.join(output_dir, "gens"), exist_ok=True)
 
-criterion = nn.BCEWithLogitsLoss()
+criterion = nn.MSELoss()
 g_optimizer = Adam(generator.parameters(), lr=args.lr, betas=(0.5, 0.999))
 d_optimizer = Adam(discriminator.parameters(), lr=args.lr, betas=(0.5, 0.999))
 
 # Training
 g_losses, d_losses, total_losses = [], [], []
 gen_images = []
+
 
 for i in range(args.epochs):
     epoch_g_loss = epoch_d_loss = epoch_total_loss = n_batches = 0
@@ -109,7 +132,7 @@ for i in range(args.epochs):
         # Train discriminator
         d_loss = 0
         for _ in range(args.k):
-            z = torch.randn(bs, 100, device=device)
+            z = torch.randn(bs, args.latent_dim, 1, 1, device=device)
             fake = generator(z)
             real_logits, fake_logits = discriminator(data), discriminator(fake.detach())
             disc_loss = criterion(real_logits, pos_labels) + criterion(
@@ -123,7 +146,7 @@ for i in range(args.epochs):
         d_loss = d_loss.mean()
 
         # Train generator
-        z = torch.randn(bs, 100, device=device)
+        z = torch.randn(bs, args.latent_dim, 1, 1, device=device)
         fake = generator(z)
         real_logits = discriminator(data)
         fake_logits = discriminator(fake)
@@ -157,7 +180,7 @@ for i in range(args.epochs):
     # Generate samples
     if (i + 1) % args.interval == 0:
         with torch.no_grad():
-            z = torch.randn(100, 100, device=device)
+            z = torch.randn(100, args.latent_dim, 1, 1, device=device)
             samples = generator(z).cpu()
 
             fig, axs = plt.subplots(10, 10, figsize=(20, 20))
